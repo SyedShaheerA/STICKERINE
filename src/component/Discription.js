17eaed5daa0e014b031f710c2dc2fd1ce730b802
Syed Shaheer Ali <shaheeraliali@hotmail.com>
2024-03-11 23:46:55 +0500

    main

--- src/component/Discription.js
+++ src/component/Discription.js
@@ -2,10 +2,27 @@ import './Discription.css'
 const Discription = (props) => {
     return(
         <>
-            <div className={props.cname}>
-                <h1>{props.title}</h1>
-                <p className="para">{props.text}</p>
-            </div>
+                <div className={props.cname}>
+                    <h1>{props.title}</h1>
+                    <p className="para">{props.text}</p>
+                </div>
+                <div className="FirstSticker">
+                    <div className='paragrap'>
+                        <h2>{props.title2}</h2>
+                        <p>
+                        {props.text2}
+                        </p>
+                    </div>
+
+                    
+
+                    <div class="Image">
+                        <img src={props.image2}></img>
+                        <img src={props.image3}></img>
+                    </div>
+                </div>
+                
+       
         </>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -16,7 +16,12 @@ function Home(){
     <Discription
         cname="Discription"
         title="STICKERINE"
-        text= "Welcome to Stickerine, your one-stop destination for turning your imagination into reality! At Stickerine, we empower you to express your unique style and ideas by creating personalized custom stickers. From vibrant designs to meaningful messages, our online platform allows you to effortlessly design and order custom stickers tailored to your preferences. Unleash your creativity, make your mark, and bring your vision to life with Stickerine – where every sticker tells a story. Explore endless possibilities and start crafting your custom stickers today!"
+        text= "Ignite Your Presence: Transform Your Image Today!"
+
+        title2="Pixel Arts"
+        text2="Welcome to Stickerine, your one-stop destination for turning your imagination into reality! At Stickerine, we empower you to express your unique style and ideas by creating personalized custom stickers. From vibrant designs to meaningful messages, our online platform allows you to effortlessly design and order custom stickers tailored to your preferences. Unleash your creativity, make your mark, and bring your vision to life with Stickerine – where every sticker tells a story. Explore endless possibilities and start crafting your custom stickers today!"
+        image2="https://drct.pixilart.com/sizes/sr2e24c59cf44aws3_400.png"
+        image3="https://art.pixilart.com/sr22082bc7393aws3.gif"
     />
     </>)
 }
